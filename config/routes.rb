51d6070fd5dbd5fd9aa829d93d511bd4dfef0d5e
Micah Young <micah@micahyoung.net>
2011-06-02 20:08:22 -0700

    Fixed another taxon page conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   
   namespace :admin do
+		# callback for jQuery sort action
     match 'taxons/reorder_products/:id' => 'taxons#reorder_products', :via => :put
        
-    resources :product_taxons do #rename to avoid conflict with products/taxons/
-      member do
-        get :select
-      end
+    resources :product_taxons do
       collection do
         get :positions
       end
